329224cdc3ddce124a8f584a51d7762894dfe4d9
Sajal Madan <57214976+Sajalmadan09@users.noreply.github.com>
2026-04-28 11:10:03 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 10
+print(type(variable))
+
+variable = 10.3
+print(type(variable))
+
+variable = False
+print(type(variable))
+
+variable = "ggwp"
+print(type(variable))
+
+variable = []
+print(type(variable))
